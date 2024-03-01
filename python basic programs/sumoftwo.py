a5583e557e64ce9271aa417818a3b57af5b4eb24
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-03-01 12:18:07 +0530

    Add files via upload
    basic program of python

---
+++ python basic programs/sumoftwo.py
@@ -0,0 +1,13 @@
+first_number = int(input("enter first number"))
+second_number = int(input("enter second number"))
+sumoftwo = first_number +second_number
+print("sum of two number is ",sumoftwo)
+# area of squar
+
+side =int(input("enter side of square"))
+areaofsquar = side*side
+print(areaofsquar)
+#average of two floating numbers
+one=float(input("enter first number"))
+two=float(input("enter second number"))
+print("average of two numbers is ", (one+two)/2)
