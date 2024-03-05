9e71ac0f06123e4257259668fff94c16d978a453
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-03-05 19:30:46 +0530

    list-functions

---
+++ python basic programs/exam.py
@@ -0,0 +1,18 @@
+#append function
+"""
+movies=[]
+mov1=input("movie one")
+mov2=input("movie two")
+mov3=input("movie three")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)"""
+#palindrome
+list1 =["m","j","m"]
+copy_list = list1.copy()
+copy_list.reverse()
+if(copy_list == list1):
+    print("palendrome")
+else:
+    print("not palendrome")
