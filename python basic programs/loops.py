ff6b8db7627153eb8a68256ae592d6bd48fcc538
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-03-07 20:30:25 +0530

    basic problems solved using loops

---
+++ python basic programs/loops.py
@@ -0,0 +1,25 @@
+#question 1
+"""
+n = 1
+while(n<=100):
+    print(n)
+    n+=1
+    #question 2
+    print("\n")
+i =100
+while(i>=1):
+    print(i)
+    i-=1
+#question 3
+a=1
+num =int(input("enter any number"))
+while(a<=10):
+    print(num*a)
+    a+="""
+#question 4
+num =[1,4,16,25,36,49,64,81,100]
+idx =0
+while(idx < len(num)):
+    print(num[idx])
+    idx+=1
+    
